5dad090d9fe3cf16d0e7cd2feaff7236f71921ee
rupaliwaghaye1 <rupaliwaghaye19@gmail.com>
2025-02-24 14:18:46 +0530

    even odd program added

---
+++ src/main/java/rupali/practice/i/can/crack/code/EvenOddNumber.java
@@ -0,0 +1,11 @@
+package rupali.practice.i.can.crack.code;
+
+public class EvenOddNumber {
+    public static void main(String[] args) {
+        int number = 156;
+        if(number % 2== 0){
+            System.out.println("Number is even");
+        }
+        else{System.out.println("Number is odd");}
+    }
+}
---
+++ src/main/java/rupali/practice/i/can/crack/code/Main.java
@@ -0,0 +1,7 @@
+package rupali.practice.i.can.crack.code;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
